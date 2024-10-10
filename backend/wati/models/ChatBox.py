296780fd16673b34ddbf26e084f668083fb2b7e5
Jayaditya Babar <165936634+Jayaditya79@users.noreply.github.com>
2024-10-10 19:46:02 +0530

    Update ChatBox.py

--- backend/wati/models/ChatBox.py
+++ backend/wati/models/ChatBox.py
@@ -1,39 +1,3 @@
-# from sqlalchemy import Column, Integer, String, Text, DateTime , Boolean
-# from sqlalchemy.ext.declarative import declarative_base
-# from datetime import datetime
-# from ..database import database
-# from . import User
-
-# Base = declarative_base()
-
-# class Conversation(database.Base):
-#     __tablename__ = 'conversations'
-    
-#     id = Column(Integer, primary_key=True, index=True)
-#     wa_id = Column(String, index=True)  # WhatsApp ID of the user
-#     message_id = Column(String)  # Unique message ID
-#     # message_id = Column(String, unique=True)  # Unique message ID
-#     phone_number_id = Column(String)  # Phone number ID
-#     message_content = Column(Text)  # Message body
-#     timestamp = Column(DateTime, default=datetime.utcnow)  # Message timestamp
-#     context_message_id = Column(String, nullable=True)  # ID of the message that this is replying to
-#     message_type = Column(String)  # Type of message (e.g., text, image)
-
-# class First_Conversation(database.Base):
-#     __tablename__ = 'first_conversations'
-
-#     id = Column(Integer, primary_key=True, autoincrement=True)
-#     business_account_id = Column(String, nullable=False)  # ID of the business account
-#     sender_wa_id = Column(String, nullable=False)  # WhatsApp ID of the sender
-#     receiver_wa_id = Column(String, nullable=False)  # WhatsApp ID of the recipient
-#     first_chat_time = Column(DateTime)  # Timestamp for the first chat
-#     active = Column(Boolean, default=True)  # Status of the conversation
-
-#     def __init__(self, business_account_id, sender_wa_id, receiver_wa_id):
-#         self.business_account_id = business_account_id
-#         self.sender_wa_id = sender_wa_id
-#         self.receiver_wa_id = receiver_wa_id
-#         self.first_chat_time = datetime.now()
 
 from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
@@ -51,9 +15,11 @@ class Conversation(database.Base):
     phone_number_id = Column(String)  # Phone number ID
     message_content = Column(Text)  # Message body
     timestamp = Column(DateTime, default=datetime.utcnow)  # Message timestamp
-    context_message_id = Column(String, nullable=True)  # ID of the message that this is replying to
+    context_message_id = Column(String, nullable=True,default= None)  # ID of the message that this is replying to
     message_type = Column(String)  # Type of message (e.g., text, image)
     is_first_message = Column(Boolean, default=False)  # Indicates if it's the first message in the conversation
+    direction = Column(String, nullable=False)  # Track message direction
+
 
 class First_Conversation(database.Base):
     __tablename__ = 'first_conversations'
