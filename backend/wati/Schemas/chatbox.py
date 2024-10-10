fad65bf7e1e22e5e8fcf937beb86e437f4ca43d3
Jayaditya Babar <165936634+Jayaditya79@users.noreply.github.com>
2024-10-10 19:46:58 +0530

    Update chatbox.py

--- backend/wati/Schemas/chatbox.py
+++ backend/wati/Schemas/chatbox.py
@@ -1,43 +1,4 @@
-# from pydantic import BaseModel , Field
-# from typing import Optional
 
-# class WebhookData(BaseModel):
-#     messaging_product: str
-#     phone_number_id: str
-#     wa_id: str
-#     message_id: str
-#     text: str
-#     timestamp: int
-#     context_message_id: Optional[str] = Field(None)
-#     message_type: str
-
-# # Define Pydantic models for the payload
-# class MessagePayload(BaseModel):
-#     phone_number_id: str
-#     wa_id: str
-#     body: str
-#     access_token: str  
-
-# from pydantic import BaseModel, Field
-# from typing import Optional
-
-# class WebhookData(BaseModel):
-#     messaging_product: str
-#     phone_number_id: str
-#     wa_id: str
-#     message_id: str
-#     text: str
-#     timestamp: int
-#     context_message_id: Optional[str] = Field(None)
-#     message_type: str
-#     is_first_message: bool = False  # Indicates if it's the first message in the conversation
-
-# # Define Pydantic models for the payload
-# class MessagePayload(BaseModel):
-#     phone_number_id: str
-#     wa_id: str
-#     body: str
-#     access_token: str
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -46,10 +7,15 @@ class WebhookData(BaseModel):
     phone_number_id: str
     wa_id: str
     message_id: str
+    # text=payload.body,  # Use 'text' as the alias
+
     message_content: str = Field(alias="text")  # Alias 'text' for incoming data
     timestamp: int
     context_message_id: Optional[str] = Field(None)
     message_type: str
+    message_type="text"
+    direction: Optional[str] = None  # Could be "sent" or "received"
+
     is_first_message: bool = False  # Indicates if it's the first message in the conversation
 
 # Define Pydantic model for the payload
